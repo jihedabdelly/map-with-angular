0f263a3d5a8362ea600ac71fe7fbbaacef8e30da
jihedabdelly <jihednew7@gmail.com>
2020-01-16 20:11:44 +0100

    map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'map-with-angular';
+  latitude = 53.05370;
+  longitude = -0.14;
+  locationChosen = false;
+  defaultAddress = "roma";
+  searchedAddress = ""
+
+  onChoseLocation(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.locationChosen = true;
+  }
+
+  onUpdateAddress(event) {
+    this.defaultAddress = event.target.value;
+  }
+ 
+  onAddAddress() {
+    this.searchedAddress = this.defaultAddress
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD-SZ0uidEWLTdm0Q7_cHE5dBNFrrLhxOo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
